f8702364952cfe9541a2c6df30ec68a586127b97
Sunday Ogbonna <oliviamegan11@gmail.com>
2024-04-08 13:30:49 +0100

    chore: reset tab two

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,18 +1,11 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 export default function TabTwoScreen() {
   return (
     <View className="flex-1">
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text className="text-center">Tab Two</Text>
     </View>
   );
 }
